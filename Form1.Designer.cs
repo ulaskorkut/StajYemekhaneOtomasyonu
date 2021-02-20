5dee5e96e22a4cfa8b87c75014e586eb6a5b599e
ulaskorkut <67353434+ulaskorkut@users.noreply.github.com>
2021-02-20 15:12:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace StajProjesi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textAnayemek = new System.Windows.Forms.TextBox();
+            this.textArayemek = new System.Windows.Forms.TextBox();
+            this.textTatli = new System.Windows.Forms.TextBox();
+            this.textIcecek = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuDataSet = new StajProjesi1.menuDataSet();
+            this.menuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuTableAdapter = new StajProjesi1.menuDataSetTableAdapters.menuTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(33, 24);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tarih";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ana Yemek";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ara Yemek";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tatlı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "İçecek";
+            // 
+            // textAnayemek
+            // 
+            this.textAnayemek.Location = new System.Drawing.Point(108, 258);
+            this.textAnayemek.Name = "textAnayemek";
+            this.textAnayemek.Size = new System.Drawing.Size(100, 20);
+            this.textAnayemek.TabIndex = 7;
+            // 
+            // textArayemek
+            // 
+            this.textArayemek.Location = new System.Drawing.Point(108, 285);
+            this.textArayemek.Name = "textArayemek";
+            this.textArayemek.Size = new System.Drawing.Size(100, 20);
+            this.textArayemek.TabIndex = 8;
+            // 
+            // textTatli
+            // 
+            this.textTatli.Location = new System.Drawing.Point(108, 311);
+            this.textTatli.Name = "textTatli";
+            this.textTatli.Size = new System.Drawing.Size(100, 20);
+            this.textTatli.TabIndex = 9;
+            // 
+            // textIcecek
+            // 
+            this.textIcecek.Location = new System.Drawing.Point(108, 337);
+            this.textIcecek.Name = "textIcecek";
+            this.textIcecek.Size = new System.Drawing.Size(100, 20);
+            this.textIcecek.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(209, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(108, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(359, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(429, 210);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // menuDataSet
+            // 
+            this.menuDataSet.DataSetName = "menuDataSet";
+            this.menuDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // menuBindingSource
+            // 
+            this.menuBindingSource.DataMember = "menu";
+            this.menuBindingSource.DataSource = this.menuDataSet;
+            // 
+            // menuTableAdapter
+            // 
+            this.menuTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(66, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(416, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 200);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ASAT Genel Müdürlüğü";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.monthCalendar1);
+            this.groupBox2.Location = new System.Drawing.Point(37, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(299, 200);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Takvim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textIcecek);
+            this.Controls.Add(this.textTatli);
+            this.Controls.Add(this.textArayemek);
+            this.Controls.Add(this.textAnayemek);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ASAT Yemekhane Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textAnayemek;
+        private System.Windows.Forms.TextBox textArayemek;
+        private System.Windows.Forms.TextBox textTatli;
+        private System.Windows.Forms.TextBox textIcecek;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private menuDataSet menuDataSet;
+        private System.Windows.Forms.BindingSource menuBindingSource;
+        private menuDataSetTableAdapters.menuTableAdapter menuTableAdapter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StajProjesi1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void MusteriGetir()
+        {
+            baglanti = new SqlConnection("Data Source=DESKTOP-B2JKE9F\\SQLEXPRESS;Initial Catalog=menu;Integrated Security=True");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT *FROM menu", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            MusteriGetir();
+        }
+
+
+
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox2.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textAnayemek.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textArayemek.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textTatli.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textIcecek.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textAnayemek.Text == "" || textArayemek.Text == "" || textTatli.Text == "" || textIcecek.Text == "")
+
+                MessageBox.Show("Lüften tarihi seçiniz yada\nverileri girdiğinizden emin olun.");
+            else
+            {
+                string sorgu = "INSERT INTO menu(tarih,anayemek,arayemek,tatli,icecek) VALUES (@tarih,@anayemek,@arayemek,@tatli,@icecek)";
+                komut = new SqlCommand(sorgu, baglanti);
+                //  komut = new SqlCommand("TRUNCATE TABLE menu", baglanti);
+                komut.Parameters.AddWithValue("@tarih", textBox1.Text);
+                komut.Parameters.AddWithValue("@anayemek", textAnayemek.Text);
+                komut.Parameters.AddWithValue("@arayemek", textArayemek.Text);
+                komut.Parameters.AddWithValue("@tatli", textTatli.Text);
+                komut.Parameters.AddWithValue("@icecek", textIcecek.Text);
+                MessageBox.Show("kayıt eklendi");
+                textBox1.Text = "";
+                textAnayemek.Text = "";
+                textArayemek.Text = "";
+                textTatli.Text = "";
+                textIcecek.Text = "";
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MusteriGetir();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string sorgu = "SELECT * from menu where tarih=@tarih";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@tarih", textBox1.Text);
+            da = new SqlDataAdapter(komut);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                string date = dr["tarih"].ToString() + " " ;
+                dr.Close();
+                DialogResult durum = MessageBox.Show(date + " Tarihinde kayıtlı olan menüyü silmek istediğinizden eminmisiniz? ", "Silme Onayı", MessageBoxButtons.YesNo);
+                if (DialogResult.Yes == durum)// Eğer evet seçilmişse kaydı silecek kodlar çalıştırılır. 
+                {
+                    string silme = "DELETE from menu where tarih=@tarih";// girilen tarihteki menüyü siler. 
+                    SqlCommand silmeKomutu = new SqlCommand(silme, baglanti);// veritabanı üzerinde sorgulama, ekleme, güncelleme, silme işlemlerini yapmak için kullanılmaktadır. 
+                    silmeKomutu.Parameters.AddWithValue("@tarih", textBox1.Text);// girilen tarihle textboxtaki tarih arasında ilişkilendirir. 
+                    silmeKomutu.ExecuteNonQuery();//Yazdığımız Verileri Çalıştıran ve İşleve Sokan parametre  
+
+                    MessageBox.Show("Kayıt Silindi.");
+
+                    textBox1.Text = "";
+                    textAnayemek.Text = "";
+                    textArayemek.Text = ""; // bir sonraki işlemler için textboxlar boşaltıldı. 
+                    textTatli.Text = "";
+                    textIcecek.Text = "";
+
+                }
+                else
+                    MessageBox.Show("Kayıt Bulunamadı.");// kayıt bulunamadığı durumlarda kullanıcı bilgilendirildi 
+                baglanti.Close();
+
+                SqlDataAdapter daa = new SqlDataAdapter("Select * From menu", baglanti);// menu adındaki tablo databaseden çekildi. 
+                DataSet ds = new DataSet();// bir kez bağlandıktan sonra veriyi alır ve bağlantıyı keser. 
+                daa.Fill(ds, "menu");  // datasetten alınan veriler dolduruldu 
+                DataTable menu = new DataTable();// doldurulan veriler menu adında bir datatable a atandı 
+                daa.Fill(menu); // menu adındaki değişkene veriler dolduruldu  
+                dataGridView1.DataSource = menu;// menu adındaki değişkene atanan veriler datagride aktarılarak gösterilmesi sağlandı. 
+                baglanti.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE menu SET tarih=@tarih,anayemek=@anayemek,arayemek=@arayemek,tatli=@tatli,icecek=@icecek WHERE id=@id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@id", Convert.ToInt32(textBox2.Text));
+            komut.Parameters.AddWithValue("@tarih", textBox1.Text);
+            komut.Parameters.AddWithValue("@anayemek", textAnayemek.Text);
+            komut.Parameters.AddWithValue("@arayemek", textArayemek.Text);
+            komut.Parameters.AddWithValue("@tatli", textTatli.Text);
+            komut.Parameters.AddWithValue("@icecek", textIcecek.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MusteriGetir();
+        }
+    }
+}
